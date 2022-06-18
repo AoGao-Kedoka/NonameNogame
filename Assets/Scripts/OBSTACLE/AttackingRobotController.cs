354ac572862e44dae202a669648155b8e32b88b8
Ao Gao <ao.gao@tum.de>
2022-06-18 12:36:53 +0200

    Update tutorial scene

--- Assets/Scripts/OBSTACLE/AttackingRobotController.cs
+++ Assets/Scripts/OBSTACLE/AttackingRobotController.cs
@@ -2,19 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class AttackingRobotController : MonoBehaviour
 {
-    [SerializeField] private int loopDuration;
-    private void Start()
+    [SerializeField] private float _overrideSpeed;
+    private GameObject _chasingRobots;
+    private PlayerInputAction _playerInputAction;
+    private bool overrided = false;
+
+    private void Awake()
     {
-        this.transform.DOLocalMoveX(transform.position.x + 20, loopDuration)
-            .SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        _playerInputAction = new PlayerInputAction();
     }
 
-    public void GetHacked()
+    private void OnEnable()
     {
-       //TODO 
+        _playerInputAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInputAction.Disable();
+        _playerInputAction.PLAYER.DEBUG.started -= GetOverride;
+    }
+    private void Start()
+    {
+        _playerInputAction.PLAYER.DEBUG.started += GetOverride;
+        _chasingRobots = GameObject.Find("ChasingRobot");
+    }
+
+    private void Update()
+    {
+        if (overrided)
+        {
+            transform.position -= new Vector3(_overrideSpeed, 0, 0);
+        }
+    }
+
+    public void GetOverride(InputAction.CallbackContext context)
+    {
+        Debug.Log("Override called");
+        overrided = true;
+        Physics2D.IgnoreLayerCollision(this.gameObject.layer, _chasingRobots.gameObject.layer, true);
     }
 }
